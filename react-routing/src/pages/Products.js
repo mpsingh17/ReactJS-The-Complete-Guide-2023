30e69894d8ba08644ae72b4e288a59695346050f
Manpreet <mpsingh17@outlook.co.nz>
2023-08-15 23:03:05 +1200

    Navigate between components using Link component.

--- react-routing/src/pages/Products.js
+++ react-routing/src/pages/Products.js
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
 const ProductsPage = () => {
-  return <h1>Products Page</h1>;
+  return (
+    <>
+      <h1>Products Page</h1>
+      <Link to="/">Back to home</Link>
+    </>
+  );
 };
 
 export default ProductsPage;
