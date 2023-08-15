9e0f08d38067b71e498ed41690e547dbd1b5416f
Manpreet <mpsingh17@outlook.co.nz>
2023-08-15 23:32:35 +1200

    Create a root layout with main navbar.

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -5,6 +5,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/RootLayout";
 
 /**
  * Define all application routes.
@@ -12,8 +13,14 @@ import ProductsPage from "./pages/Products";
  * @returns router
  */
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 function App() {
