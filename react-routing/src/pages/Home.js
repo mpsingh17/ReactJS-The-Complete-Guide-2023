66957968c67f23cf1248056d35ec06e5cc2d72fe
Manpreet <mpsingh17@outlook.co.nz>
2023-08-16 11:26:08 +1200

    navigation programatically using useNavigate hook.

--- react-routing/src/pages/Home.js
+++ react-routing/src/pages/Home.js
@@ -1,12 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  // Call useNavigate hook from react router dom to get a function reference.
+  const navigate = useNavigate();
+
+  /**
+   * Use the function reference from useNavigate hook call to programatically navigate the user.
+   * This code here is just for demonstration of the useNavigate hook functionality.
+   */
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>Home Page.</h1>
       <p>
         Go to <Link to="/products">list of products.</Link>
       </p>
+
+      <button onClick={navigateHandler}>Navigate to products</button>
     </>
   );
 };
