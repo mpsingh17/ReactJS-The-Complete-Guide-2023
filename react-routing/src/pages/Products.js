f1c725aba17d754a86ab5bbff7df4d83735fbc14
Manpreet <mpsingh17@outlook.co.nz>
2023-08-16 12:43:36 +1200

    Generate links for dynamic routes.

--- react-routing/src/pages/Products.js
+++ react-routing/src/pages/Products.js
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
+
 const ProductsPage = () => {
+  const PRODUCTS = [
+    { productId: "1", title: "Product 1" },
+    { productId: "2", title: "Product 2" },
+    { productId: "3", title: "Product 3" },
+    { productId: "4", title: "Product 4" },
+  ];
+
   return (
     <>
       <Link to="/">Back to home</Link>
 
       <h1>Products Page</h1>
       <ul>
-        <li>
-          <Link to="/products/p1">Product 1</Link>
-        </li>
-        <li>
-          <Link to="/products/p2">Product 2</Link>
-        </li>
-        <li>
-          <Link to="/products/p3">Product 3</Link>
-        </li>
+        {PRODUCTS.map((product) => (
+          <li key={product.productId}>
+            <Link to={`/products/${product.productId}`}>{product.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
