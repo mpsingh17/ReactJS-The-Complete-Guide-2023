a2915214dfb2748f295bd9607a1bf5d0e283cb14
Manpreet <mpsingh17@outlook.co.nz>
2023-08-17 16:37:00 +1200

    display a text loader when loading events.

--- react-routing-advanced/frontend/src/pages/Layout.js
+++ react-routing-advanced/frontend/src/pages/Layout.js
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 const Layout = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
-      <Outlet />
+      <main>
+        {navigation.state === "loading" && <p>Loading...</p>}
+        <Outlet />
+      </main>
     </>
   );
 };
