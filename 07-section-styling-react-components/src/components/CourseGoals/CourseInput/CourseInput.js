00b6867bfaef44b3bf7c45630a1d0fafa232263c
Manpreet <mpsingh17@outlook.co.nz>
2023-08-22 11:53:19 +1200

    apply CSS classes dynamically.

--- 07-section-styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 07-section-styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -27,16 +27,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler}
-          style={{
-            borderColor: !isValid ? "red" : "black",
-            background: !isValid ? "salmon" : "transparent",
-          }}
-        />
+      <div className={`form-control ${!isValid ? "invalid" : null}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
