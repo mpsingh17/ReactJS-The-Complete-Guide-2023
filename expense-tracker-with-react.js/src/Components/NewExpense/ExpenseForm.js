f1368da419f3fe4774d44b1759241a97d5256c8d
Manpreet <mpsingh17@outlook.co.nz>
2023-08-21 20:50:36 +1200

    convert amount entered to number.

--- expense-tracker-with-react.js/src/Components/NewExpense/ExpenseForm.js
+++ expense-tracker-with-react.js/src/Components/NewExpense/ExpenseForm.js
@@ -31,7 +31,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
