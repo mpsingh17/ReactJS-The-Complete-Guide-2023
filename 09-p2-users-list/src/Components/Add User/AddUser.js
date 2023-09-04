af70d847f834cffa9e3558e2ae1580c662aa1735
Manpreet <mpsingh17@outlook.co.nz>
2023-09-04 12:28:59 +1200

    Add user form.

--- 09-p2-users-list/src/Components/Add User/AddUser.js
+++ 09-p2-users-list/src/Components/Add User/AddUser.js
@@ -1,12 +1,51 @@
-const AddUser = () => {
+import { useState } from "react";
+
+const initialUser = {
+  id: Math.random(),
+  userName: "",
+  userAge: "",
+};
+
+const AddUser = (props) => {
+  const [userDetails, setUserDetails] = useState(initialUser);
+
+  const onInputChangeHandler = (inputId, value) => {
+    setUserDetails((prevState) => {
+      return {
+        ...prevState,
+        [inputId]: value,
+      };
+    });
+  };
+
+  const onSubmitHandler = (event) => {
+    setUserDetails(initialUser);
+    props.onAddUserHandler(event, userDetails);
+  };
+
   return (
     <>
       <h1>Add User</h1>
-      <form>
+
+      <form onSubmit={onSubmitHandler}>
         <label>User Name</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={userDetails.userName}
+          onChange={(event) =>
+            onInputChangeHandler("userName", event.target.value)
+          }
+        />
+
         <label>User Age</label>
-        <input type="number" />
+        <input
+          type="number"
+          value={userDetails.userAge}
+          onChange={(event) =>
+            onInputChangeHandler("userAge", event.target.value)
+          }
+        />
+
         <div>
           <button type="submit">Add User</button>
         </div>
