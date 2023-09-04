b4863e16e0e1bc9e3567548a057ad4998a5cccd8
Manpreet <mpsingh17@outlook.co.nz>
2023-09-04 12:29:33 +1200

    Display add user form and user list.

--- 09-p2-users-list/src/App.js
+++ 09-p2-users-list/src/App.js
@@ -1,11 +1,28 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import AddUser from "./Components/Add User/AddUser";
+import UserList from "./Components/UserList/UserList";
+
+const initialUsersList = [];
 
 function App() {
+  const [userList, setUserList] = useState(initialUsersList);
+
+  const onAddUserHandler = (event, userDetails) => {
+    event.preventDefault();
+
+    const userToAdd = { ...userDetails, id: Math.random() };
+
+    setUserList((prevState) => {
+      return [...prevState, userToAdd];
+    });
+  };
+
   return (
     <div className="App">
-      <AddUser />
+      <AddUser onAddUserHandler={onAddUserHandler} />
+      <p>{userList.length}</p>
+      <UserList userList={userList} />
     </div>
   );
 }
